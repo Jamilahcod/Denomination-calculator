e9c16a28f1ef65a6af3ad9bf182f1af248c2e9de
Jamilahcod <jamilahmardana2008@gmail.com>
2025-03-29 09:53:59 +0400

    denomination calculator

---
+++ calculator.py
@@ -0,0 +1,7 @@
+from tkinter import *
+from tkinter import messagebox
+from PIL import Image,ImageTk
+root=Tk()
+root.title("denomination calculator")
+root.configure(bg="light blue")
+root.geometry('650x400')
